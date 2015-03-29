36e0019a98a5f448e89b911d7ef8142558d74895
Mohamed Omer <mohamed.omer@ericsson.com>
2015-03-29 08:58:49 +0200

    added user model

--- Gemfile
+++ Gemfile
@@ -5,7 +5,7 @@ source 'http://rubygems.org'
 gem 'rails', '4.1.8'
 # Use sqlite3 as the database for Active Record
     group :production do
-	gem 'pg'
+	      gem 'pg'
     end
     group :development, :test do
         gem 'sqlite3'
--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150319233244) do
+ActiveRecord::Schema.define(version: 20150320163308) do
 
   create_table "languages", force: true do |t|
     t.string   "name"
     t.text     "text"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "email"
+    t.string   "password"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
